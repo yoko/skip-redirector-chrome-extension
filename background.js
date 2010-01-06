319d591bec836810e8ae9352995720bdec706782
yksk <yoko@carbonfairy.org>
2010-01-06 11:50:32 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,48 @@
+
+const API = 'http://wedata.net/databases/Redirector/items.json';
+
+chrome.extension.onRequest.addListener(function(request, sender, callback) {
+	var xhr, timer;
+	var stash = load();
+	if (stash && stash.expires >= +new Date)
+		callback(stash.data);
+	else {
+		xhr = new XMLHttpRequest;
+		xhr.open('GET', API, true);
+		xhr.onload = function() {
+			console.log('loaded', xhr);
+			clearTimeout(timer);
+			var data = JSON.parse(xhr.responseText);
+			save(data);
+			callback(data);
+		};
+		xhr.onerror = function() {
+			console.log('request error', xhr);
+		};
+		xhr.send();
+
+		if (stash)
+			timer = setTimeout(function() {
+				console.log('timeout');
+				xhr.abort();
+				callback(save(stash.data));
+			}, 1000 * 30);
+	}
+});
+
+
+function load() {
+	return localStorage.stash && JSON.parse(localStorage.stash);
+}
+
+function save(data) {
+	if (data)
+		localStorage.stash = JSON.stringify({
+			data   : data,
+			expires: +new Date + 1000 * 60 * 60 * 24
+		});
+	else
+		delete localStorage.stash;
+
+	return data;
+}
---
+++ skip_redirector.js
@@ -0,0 +1,49 @@
+
+chrome.extension.sendRequest(null, function(data) {
+	var i = data.length;
+	while (i--) {
+		var item = data[i].data;
+		var url = item.url;
+		var link = item.link;
+		var replace_url = item.replace_url;
+		if (new RegExp(url).test(location.href)) {
+			if (replace_url) {
+				var reditrect_url = decodeURIComponent(location.href.replace(new RegExp(url), replace_url));
+				if (/^https?:\/\//.test(reditrect_url))
+					location.href = reditrect_url;
+			}
+			else if (link) {
+				var a = $X(link)[0];
+				if (a) {
+					var e = document.createEvent('MouseEvent');
+					e.initEvent('click', false, true);
+					a.dispatchEvent(e);
+				}
+			}
+			return;
+		}
+	}
+});
+
+
+// http://gist.github.com/3242
+function $X (exp, context) {
+	context || (context = document);
+	var expr = (context.ownerDocument || context).createExpression(exp, function (prefix) {
+		return document.createNSResolver(context.documentElement || context).lookupNamespaceURI(prefix) ||
+			context.namespaceURI || document.documentElement.namespaceURI || "";
+	});
+
+	var result = expr.evaluate(context, XPathResult.ANY_TYPE, null);
+		switch (result.resultType) {
+			case XPathResult.STRING_TYPE : return result.stringValue;
+			case XPathResult.NUMBER_TYPE : return result.numberValue;
+			case XPathResult.BOOLEAN_TYPE: return result.booleanValue;
+			case XPathResult.UNORDERED_NODE_ITERATOR_TYPE:
+				// not ensure the order.
+				var ret = [], i = null;
+				while (i = result.iterateNext()) ret.push(i);
+				return ret;
+		}
+	return null;
+}
